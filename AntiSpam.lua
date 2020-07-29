c61a134e9e8629aac5d5b7a1fcf6d17d10bf5714
outerhavenphoenix <outerhavenphoenix@gmail.com>
2020-07-29 12:13:06 -0500

    Anti Click Detector Spam

---
+++ AntiSpam.lua
@@ -0,0 +1,30 @@
+--// AntiSpam eeyeye (Phoenix)
+local AntiSpam = {} -- Index: <instance> Player | Key: <int> Number of attempts
+local DelayQueue = {} -- Index: <instance> Player | Key: <boolean> Delaying
+
+local Wrap = function(Function) --// Function that prevents spam for global controls eeeeyeeet
+    local Delay = function(Player)
+        DelayQueue[Player] = true
+        
+        wait(5)
+        
+        DelayQueue[Player] = false
+        AntiSpam[Player] = 0
+    end
+    
+    return function(Player)
+        if AntiSpam[Player] == nil then
+            AntiSpam[Player] = 0
+        end
+        
+        if DelayQueue[Player] == nil then
+            DelayQueue[Player] = false
+        end
+
+        if AntiSpam[Player] < 5 then
+            Function(Player)
+        elseif AntiSpam[Player] > 5 and DelayQueue[Player] == false then
+            Delay(Player)
+        end
+    end
+end
