ad75ca69ea40465c070f9fbdd04db1df544fcec5
outerhavenphoenix <outerhavenphoenix@gmail.com>
2020-07-30 07:50:43 -0500

    Fixed QSERF Only bug eeee

--- ServerPrintDebug.lua
+++ ServerPrintDebug.lua
@@ -1,3 +1,3 @@
-local Debug = game:GetService("ServerScriptService").GAME_VERSION.DEBUG.Value
+local Debug = false--// QSERF >> game:GetService("ServerScriptService").GAME_VERSION.DEBUG.Value
 local print = function(...) if Debug == true then for Index, Str in pairs({...}) do print("["..script.Name.."]: " .. tostring(Str)) end end end
 local warn  = function(...) for Index, Str in pairs({...}) do  warn("["..script.Name.."]: " .. tostring(Str)) end end
