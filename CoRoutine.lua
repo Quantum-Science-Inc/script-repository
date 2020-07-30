f5a497ec39c47eeb2faeae4c012d92f56553b4e2
outerhavenphoenix <outerhavenphoenix@gmail.com>
2020-07-30 07:45:45 -0500

    Updated to module script

--- CoRoutine.lua
+++ CoRoutine.lua
@@ -1,34 +1,49 @@
+--[[
+    <ModuleScript>
+
+    returns <Array>
+]]
+
 local Routines = {}
 
-local RandomID = function()
-    local String = ""
+return {
+    RandomID = function()
+        local String = ""
 
-    for I=1, Random.new(tick()):NextInteger(5, 25) do
-        String = String .. string.char(Random.new(tick()+math.random(math.random(80, 1000), math.random(5, 25))):NextInteger(097, 122)) --// LOL
-    end
-
-    return String
-end
-
-local MakeRoutine = function(Function, ...)
-    local Args = {...}
-
-    local RunFunction = function()
-        local Args = Args
-        local Function = Function
-
-        Function(unpack(Args))
-    end
-
-    local ID = RandomID()
-
-    Routines[ID] = coroutine.create(function() 
-        local Ran, Error = pcall(RunFunction)
-
-        if Error then
-            warn("Got error running Function ID: ".. tostring(ID) .." Error;\n".. tostring(Error))
+        for I=1, Random.new(tick()):NextInteger(5, 25) do
+            String = String .. string.char(Random.new(tick()+math.random(math.random(80, 1000), math.random(5, 25))):NextInteger(097, 122)) --// LOL
         end
-    end)
 
-    coroutine.resume(Routines[ID])
-end
+        return String
+    end;
+
+    MakeRoutine = function(Function, ...)
+        local Args = {...}
+
+        local RunFunction = function()
+            local Args = Args
+            local Function = Function
+
+            Function(unpack(Args))
+        end
+
+        local ID = RandomID()
+
+        Routines[ID] = coroutine.create(function() 
+            local Ran, Error = pcall(RunFunction)
+
+            if Error then
+                warn("Got error running Function ID: ".. tostring(ID) .." Error;\n".. tostring(Error))
+            end
+        end)
+
+        coroutine.resume(Routines[ID])
+    end;
+
+    Clear = function()
+        for Index, Routine in pairs(Routines) do
+            coroutine.yield(Routine)
+            Routines[Index] = nil --// GC help pls
+        end
+    end;
+}
